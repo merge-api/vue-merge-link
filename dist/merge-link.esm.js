e5573d636703a5af7422ec0c4a3ca655a7d00ce7
Elton Leong <elton@merge.dev>
2022-05-25 10:23:36 -0700

    Switch to yarn and upgrade to Vue 3

--- dist/merge-link.esm.js
+++ dist/merge-link.esm.js
@@ -1,20 +1,14 @@
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+import { openBlock, createElementBlock, renderSlot, createElementVNode } from 'vue';
+
 var script = {
   name: "MergeLink",
   props: {
     linkToken: String,
+    shouldSendTokenOnSuccessfulLink: Boolean,
     onReady: Function,
     onSuccess: Function,
-    onExit: Function
+    onExit: Function,
+    tenantConfig: Object
   },
 
   created() {
@@ -29,6 +23,8 @@ var script = {
     onScriptLoaded() {
       window.MergeLink.initialize({
         linkToken: this.linkToken,
+        tenantConfig: this.tenantConfig,
+        shouldSendTokenOnSuccessfulLink: this.shouldSendTokenOnSuccessfulLink,
         onExit: this.onExit,
         onReady: this.onReady,
         onSuccess: this.onSuccess
@@ -69,127 +65,21 @@ var script = {
   }
 };
 
-function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier /* server only */, shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
-    if (typeof shadowMode !== 'boolean') {
-        createInjectorSSR = createInjector;
-        createInjector = shadowMode;
-        shadowMode = false;
-    }
-    // Vue.extend constructor export interop.
-    const options = typeof script === 'function' ? script.options : script;
-    // render functions
-    if (template && template.render) {
-        options.render = template.render;
-        options.staticRenderFns = template.staticRenderFns;
-        options._compiled = true;
-        // functional template
-        if (isFunctionalTemplate) {
-            options.functional = true;
-        }
-    }
-    // scopedId
-    if (scopeId) {
-        options._scopeId = scopeId;
-    }
-    let hook;
-    if (moduleIdentifier) {
-        // server build
-        hook = function (context) {
-            // 2.3 injection
-            context =
-                context || // cached call
-                    (this.$vnode && this.$vnode.ssrContext) || // stateful
-                    (this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext); // functional
-            // 2.2 with runInNewContext: true
-            if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
-                context = __VUE_SSR_CONTEXT__;
-            }
-            // inject component styles
-            if (style) {
-                style.call(this, createInjectorSSR(context));
-            }
-            // register component module identifier for async chunk inference
-            if (context && context._registeredComponents) {
-                context._registeredComponents.add(moduleIdentifier);
-            }
-        };
-        // used by ssr in case component is cached and beforeCreate
-        // never gets called
-        options._ssrRegister = hook;
-    }
-    else if (style) {
-        hook = shadowMode
-            ? function (context) {
-                style.call(this, createInjectorShadow(context, this.$root.$options.shadowRoot));
-            }
-            : function (context) {
-                style.call(this, createInjector(context));
-            };
-    }
-    if (hook) {
-        if (options.functional) {
-            // register for functional component in vue file
-            const originalRender = options.render;
-            options.render = function renderWithStyleInjection(h, context) {
-                hook.call(context);
-                return originalRender(h, context);
-            };
-        }
-        else {
-            // inject component registration as beforeCreate hook
-            const existing = options.beforeCreate;
-            options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
-        }
-    }
-    return script;
+const _hoisted_1 = {
+  class: "merge-link-wrapper"
+};
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", _hoisted_1, [renderSlot(_ctx.$slots, "button", {
+    onClick: $options.handleOnClick
+  }, () => [createElementVNode("button", {
+    class: "merge-link-button",
+    onClick: _cache[0] || (_cache[0] = function () {
+      return $options.handleOnClick && $options.handleOnClick(...arguments);
+    })
+  }, [renderSlot(_ctx.$slots, "default")])])]);
 }
 
-/* script */
-const __vue_script__ = script;
-/* template */
-
-var __vue_render__ = function () {
-  var _vm = this;
-
-  var _h = _vm.$createElement;
-
-  var _c = _vm._self._c || _h;
-
-  return _c('div', {
-    staticClass: "merge-link-wrapper"
-  }, [_vm._t("button", [_c('button', {
-    staticClass: "merge-link-button",
-    on: {
-      "click": _vm.handleOnClick
-    }
-  }, [_vm._t("default")], 2)], {
-    "onClick": _vm.handleOnClick
-  })], 2);
-};
-
-var __vue_staticRenderFns__ = [];
-/* style */
-
-const __vue_inject_styles__ = undefined;
-/* scoped */
-
-const __vue_scope_id__ = undefined;
-/* module identifier */
-
-const __vue_module_identifier__ = undefined;
-/* functional template */
-
-const __vue_is_functional_template__ = false;
-/* style inject */
-
-/* style inject SSR */
-
-/* style inject shadow dom */
-
-const __vue_component__ = /*#__PURE__*/normalizeComponent({
-  render: __vue_render__,
-  staticRenderFns: __vue_staticRenderFns__
-}, __vue_inject_styles__, __vue_script__, __vue_scope_id__, __vue_is_functional_template__, __vue_module_identifier__, false, undefined, undefined, undefined);
+script.render = render;
 
 // Import vue component
 // IIFE injects install function into component, allowing component
@@ -197,7 +87,7 @@ const __vue_component__ = /*#__PURE__*/normalizeComponent({
 
 var entry_esm = /*#__PURE__*/(() => {
   // Get component instance
-  const installable = __vue_component__; // Attach install function executed by Vue.use()
+  const installable = script; // Attach install function executed by Vue.use()
 
   installable.install = Vue => {
     Vue.component('MergeLink', installable);
@@ -208,4 +98,4 @@ var entry_esm = /*#__PURE__*/(() => {
 // also be used as directives, etc. - eg. import { RollupDemoDirective } from 'rollup-demo';
 // export const RollupDemoDirective = directive;
 
-export default entry_esm;
+export { entry_esm as default };
